d48cb6d510ce4b487a66de6b14c437177373abaa
Yusuf GOKOL <ygokol@e1r8p7.42.fr>
2017-03-11 22:29:10 +0100

    ok

--- y/ft_printf.c
+++ y/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ygokol <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/14 17:54:40 by ygokol            #+#    #+#             */
-/*   Updated: 2017/03/10 18:35:33 by ygokol           ###   ########.fr       */
+/*   Updated: 2017/03/11 22:28:53 by ygokol           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,27 @@
 #include "ft_printf.h"
 #include "libft.h"
 
-int		analyze(const char *format, va_list *ap)
+int		analyze(const char *format, va_list *ap, int x)
 {
 	printf("\n__________ Analyze __________\n");
 	t_argmnt	*tmp;
-	int x = 0;
 	tmp = malloc(sizeof(t_argmnt));
-	while (format[x] != '\0')
+	while (format[x] != '\0' && format[x] != ' ')
 	{
 		if (format[x] == '%')
 		{
 			x++;
-			if (format[x] == '%')
-				ft_putchar('%');
-			parse_type(format[x], tmp);
+			parse_type((char *)format, tmp, x);
 			parse_flags(format[x], tmp);
 			parse_modif(format, tmp);
 			parse_prec(format, tmp);
 			parse_width(format, tmp);
 		}
 		else
-			ft_putchar(format[x]);
-		x++;
+			x++;
+		printf("\ncompteur: |%c|\n", format[x]);
 	}
-	
-printf("\n|s_argmnt|\narg: %s \nflag: %c \nprec: %c\nmodif: %d\ntype: %c\n_ _ _ _ _ \n", (char*)tmp->arg, tmp->flag, tmp->prec
-		,(int)tmp->modif, tmp->type);
-	
-	tmp->next = NULL;
+	printf("\narg: %s \nflag: %c \nprec: %c\nmodif: %d\ntype: %c\n_ _ _ _ _ \n", (char *)tmp->arg, tmp->flag, tmp->prec, (int)tmp->modif, tmp->type);
 	return (0);
 }
 
@@ -50,28 +43,31 @@ int		ft_printf(const char *format, ...)
 	va_list		ap;
 	int i;
 	int count;
-	
+
 	va_start(ap, format);
 	i = 0;
 	count = 0;
 	while (format[i])
 	{
-		if (format[i] == '%' && ++i)
+		if (format[i] == '%')
 		{
-			count += analyze(format, &ap);
+			analyze(format, &ap, i);
+			i++;
 		}
-		else
-			i += write(1, &format[i], 1);
-	}
+			else
+			i += write(1,&format[i],1);
+		}
 	va_end(ap);
-	return (count + i);
+	return (i);
 }
 
 int		main ()
 {
-	int i = ft_printf("ok%d %#2.5ls %p %s %%", 1, 2 , 3 , "lol");
+	int i = ft_printf("\nft_printf: ok123%d %d %s \n", 1, 2 , "lol");
+	int j = printf("\nprintf: ok123%d %d %s \n", 1, 2 , "lol");
 	//i = ft_printf("%", 1, 2 , 3 , "lol");
 	//printf("|retour : %d|\n", printf("{%ls}", L"\xF0\x9D\x84\x9E"));
-	printf("return: %d\n", i);
+	printf("return ft_printf: %d\n", i);
+	printf("return printf	: %d\n", i);
 	return (0);
 }
--- y/parsing.c
+++ y/parsing.c
@@ -6,7 +6,7 @@
 /*   By: ygokol <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/20 21:37:00 by ygokol            #+#    #+#             */
-/*   Updated: 2017/03/10 17:59:58 by ygokol           ###   ########.fr       */
+/*   Updated: 2017/03/11 22:27:23 by ygokol           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,43 +21,53 @@ void		parse_prec(const char *chr, t_argmnt *tmp)
 
 }
 
-void		parse_type(char chr, t_argmnt *tmp)
+int		parse_type(char* chr, t_argmnt *tmp, int i)
 {
-	int i;
-	i = 0;
-	printf("__ TYPE : |%c|\n", chr);
-	if (chr == 's')
-		tmp->type = 's';
-	if (chr == 'S')
-		tmp->type = 'S';
-	if (chr == 'p')
-		tmp->type= 'p';
-	if (chr == 'd')
-		tmp->type = 'd';
-	if (chr == 'D')
-		tmp->type = 'D';
-	if (chr == 'i')
-		tmp->type = 'i';
-	else
-		tmp->type = '\0';
+	int x;
+	x = i;
+
+	tmp->type = '\0';
+	while (tmp->type == '\0')
+	{
+		if (chr[x] == 's')
+			tmp->type = 's';
+		if (chr[x] == 'S')
+			tmp->type = 'S';
+		if (chr[x] == 'p')
+			tmp->type= 'p';
+		if (chr[x] == 'd')
+			tmp->type = 'd';
+		if (chr[x] == 'D')
+			tmp->type = 'D';
+		if (chr[x] == 'i')
+			tmp->type = 'i';
+		x++;
+	}
+	x = 0;
+	return (1);
 }
 
 void		parse_modif(const char *chr, t_argmnt *tmp)
 {
 	int i;
 	i = 0;
-	if (chr[i] == 'h')
-		tmp->modif = "h";
-	if (chr[i] == 'h' && chr[i + 1] == 'h')
-		tmp->modif = "hh";
-	if (chr[i] == 'l')
-		tmp->modif = "l";
-	if (chr[i] == 'l' && chr[i + 1] == 'l')
-		tmp->modif = "ll";
-	if (chr[i] == 'j')
-		tmp->modif = "j";
-	if (chr[i] == 'z')
-		tmp->modif = "z";
+	tmp->modif = NULL;
+	while (chr[i] != ' ')
+	{
+		if (chr[i] == 'h')
+			tmp->modif = "h";
+		if (chr[i] == 'h' && chr[i + 1] == 'h')
+			tmp->modif = "hh";
+		if (chr[i] == 'l')
+			tmp->modif = "l";
+		if (chr[i] == 'l' && chr[i + 1] == 'l')
+			tmp->modif = "ll";
+		if (chr[i] == 'j')
+			tmp->modif = "j";
+		if (chr[i] == 'z')
+			tmp->modif = "z";
+		i++;
+	}
 }
 
 void		parse_flags(char chr, t_argmnt *tmp)
@@ -72,4 +82,6 @@ void		parse_flags(char chr, t_argmnt *tmp)
 		tmp->flag = '+';
 	if (chr == ' ')
 		tmp->flag = ' ';
+	else
+		tmp->flag = '\0';
 }
